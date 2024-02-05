858c1c54bac407c8a786027d8e2bfd7daa6fc266
TharunTech <tharuntech9840@gmail.com>
2024-02-05 13:25:31 +0530

    Circular queue - DSA - isEmpty and isfull added

--- src/dataStructures/Queue.java
+++ src/dataStructures/Queue.java
@@ -9,20 +9,40 @@ public class Queue {
 
 	public void enqueue(int data) {
 		// TODO Auto-generated method stub
-		rear++;
+		
+		if(!isfull()) {
+		rear = (rear+1)%4;
 		arr[rear]= data;
 		size++;
+		}else {
+			System.out.println("queue is full");
+		}
 		
 	}
 	
 	public int dequeue() {
+		if(!isEmpty()) {
 		int data = arr[front];
-		front++;
+		front = (front+1)%4;
+		size--;
 		return data;
+		}
+		else {
+			System.out.println("queue is empty");
+			return -1;
+		}
 	}
 	
-	public void show() {
-		// TODO Auto-generated method stub
+	public boolean isfull() {
+		return size == 4;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	
+	public void show() {//show will not work correct as circular queue size circles and the data order will change,
+						// when we continously do enqueue and dequeue;
 		System.out.println("Queue is :");
 	
 		
